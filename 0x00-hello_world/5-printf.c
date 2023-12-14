5296f0111ee405d1f0b47ad8d12d60decd1e0e98
Jondatec <rastecsisanda@proton.me>
2023-12-14 10:12:01 -0800

    program to print a sentence with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program to print string to standard out.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
