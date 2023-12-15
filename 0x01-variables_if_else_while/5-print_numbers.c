f8d8b469313aec5c10faaa15d95f3d76b51b0e6e
Jondatec <rastecsisanda@proton.me>
2023-12-15 13:43:36 -0800

    program to print numbers to base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Definition: Program that prints numbers of base ten
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf(a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
