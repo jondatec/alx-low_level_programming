ce9fad2f7c707ce22dbb57eb9e1a1851551fc120
Jondatec <rastecsisanda@proton.me>
2023-12-15 13:20:08 -0800

    program to print alphabet in low with exceptions of some letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Definition: program prints lowercase alphabet with exception
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int low_Alph = 'a';
+
+	while (low_Alph <= 'z')
+	{
+		if ((low_Alph == 'q') || (low_Alph == e))
+			continue;
+		putchar(low_Alph);
+		low_Alph++;
+	}
+	putchar('\n');
+	return (0);
+}
