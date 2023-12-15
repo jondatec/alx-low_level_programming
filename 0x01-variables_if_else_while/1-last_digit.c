f19483fb3e8dffd1f999e6a2b16f7bb87f777c29
Jondatec <rastecsisanda@proton.me>
2023-12-15 12:43:57 -0800

    program determines if a random number is greater than 5, is zero and if is less than six and not zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,20 +10,21 @@
 int main(void)
 {
 	int n;
+	int x = n%10;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n[-1] > 5)
+	if (x > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n[-1]);
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
 	}
-	else if (n[-1] == 0)
+	else if (x == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n[-1]);
+		printf("Last digit of %d is %d and is 0\n", n, x);
 	}
-	else if ((n[-1] < 6) && (n[-1] != 0))
+	else if ((x < 6) && (x != 0))
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n[-1]);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
 	}
 	return (0);
 }
