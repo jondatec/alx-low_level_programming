f212183199bf87392a11c33d78a442d61cba1a20
Jondatec <rastecsisanda@proton.me>
2023-12-15 13:25:06 -0800

    program to print alphabet in low with exceptions of some letters

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	while (low_Alph <= 'z')
 	{
-		if ((low_Alph == 'q') || (low_Alph == 'e'))
+		if ((low_Alph == 'q') && (low_Alph == 'e'))
 			continue;
 		putchar(low_Alph);
 		low_Alph++;
