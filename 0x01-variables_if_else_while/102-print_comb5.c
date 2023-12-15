330eb20e7c4b6fbf9522b6f772860c7dbf749ab1
Jondatec <rastecsisanda@proton.me>
2023-12-15 15:03:26 -0800

    program that prints all possible combinations of two two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Definition: program that prints all combinations of
+ *             two two digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n = 48;
+	int m = 0;
+	int o;
+	int p;
+
+	while (m <= 99)
+	{
+		b=m+1;
+		while (o <= 99)
+		{
+			putchar((m / 10) + n);
+			putchar((m % 10) + n);
+			putchar(32);
+			putchar((o / 10) + n);
+			putchar((o % 10) + n1);
+			if (m != 98 || o != 99)
+			{
+				putchar(p);
+				putchar(32);
+			}
+			o += 1;
+		}
+		m += 1;
+	}
+	putchar('\n');
+	return (0);
+}
