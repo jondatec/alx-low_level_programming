298a0535f5f78439a1d91a256ad1a7aed4b9c64a
Jondatec <rastecsisanda@proton.me>
2023-12-20 08:33:51 -0800

    adding function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * Definition: function that adds two numbers
+ * @i: character 1
+ * @j: character 2
+ * Return: 1 (success)
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
