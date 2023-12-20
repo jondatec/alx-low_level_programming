050072273f990a5cb66481722cee925fe1362297
Jondatec <rastecsisanda@proton.me>
2023-12-20 01:08:05 -0800

    function checks if function is alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ *
+ * Description: this function checks if a character is alpha
+ * @c: the character to check
+ * Return: 1 (success) and 0 (otherwise)
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
