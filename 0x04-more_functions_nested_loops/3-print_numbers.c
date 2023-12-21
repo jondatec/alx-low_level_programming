1d1cf569f5ef673d03c3b24a28036373951ba022
Jondatec <rastecsisanda@proton.me>
2023-12-21 15:09:44 -0800

    function to print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Definition: function that prints numbers"
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
