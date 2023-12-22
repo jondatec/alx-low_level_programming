168620fd2319312366de12518d7c24a6b7aba2cd
Jondatec <rastecsisanda@proton.me>
2023-12-22 13:08:12 -0800

    function that draws lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - Enter point
+ *
+ * Definition: this function draws a straight  line
+ * Return: void
+ */
+void print_line(int n)
+{
+	char i = '_';
+	int j = 0;
+
+	while (j < n && n > 0)
+	{
+		_putchar(i);
+		j++;
+	}
+	_putchar('\n');
+}
