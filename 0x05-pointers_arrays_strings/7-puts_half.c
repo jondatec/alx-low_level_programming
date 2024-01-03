573939c8d432c9ae08040c283672c9e5e3927793
jondatec <rastecsisanda@proton.me>
2024-01-03 14:27:13 -0800

    function prints half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - function to print half of tha string
+ * @str: pointer to string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, j = 0;
+
+	while (str[i])
+		i++;
+	j = i/2;
+	if (i % 2 != 0)
+		j += 1;
+	while (str[j])
+	{
+		_putchar(str[j]);
+		j++;
+	}
+	_putchar('\n');
+}
