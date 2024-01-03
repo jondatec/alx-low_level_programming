1ecf05e0a46a092558f033510912bec8f3370ae2
jondatec <rastecsisanda@proton.me>
2024-01-03 12:59:26 -0800

    dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function updates value pointed to & takes pointer as par
+ * @n: pointer char to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
